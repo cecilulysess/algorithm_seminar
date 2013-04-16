0f5939de5db6809cb10a24773215338d74d12fbd
Jin Qin <ustc.bigtailwolf@gmail.com>
2013-04-15 22:37:11 -0400

    Modified Palindrome problems

--- PalindromeNumber/main.cpp
+++ PalindromeNumber/main.cpp
@@ -1,17 +1,13 @@
 #include <iostream>
 using std::cin; using std::cout; using std::endl;
 
-unsigned revert(unsigned num) {
-  unsigned rev = 0;
-  while (num) {
-    rev = rev * 10 + num % 10;
-    num /= 10;
-  }
-  return rev;
+/* Your foo function is to return whether a number is Palindrome Number */
+
+bool foo() {
 }
 
 int main(void) {
   unsigned num;
   cin >> num;
-  cout << (revert(num) == num) << endl;
+  cout << foo(num) << endl;
 }
--- PalindromeString/main.cpp
+++ PalindromeString/main.cpp
@@ -3,38 +3,9 @@ using std::cin; using std::cout; using std::endl;
 #include <string>
 using std::string;
 
+/* Your foo function is to return the maximum Palindrome substring of an input string */
+
 string foo(const string& input) {
-  int size = input.size() * 2 - 1;
-  int* count = new int[size];
-  for ( int i = 0; i < size; ++i )
-    count[i] = !(i % 2);
-
-  for ( int i = 0; i < size; ++i ) {
-    int left  = i/2 - !(i%2);
-    int right = i/2 + 1;
-
-    while ( left >= 0 and right <= size/2 and
-        input[left] == input[right] ) {
-
-      count[i] += 2;
-      --left;
-      ++right;
-    }
-  }
-
-  /* Find max point and construct output string */
-  int maxPoint = 0;
-  for ( int i = 1; i < size; ++i )
-    if ( count[i] > count[maxPoint] ) maxPoint = i;
-
-  int start = maxPoint/2 - (count[maxPoint]-1)/2;
-
-  string output;
-
-  for ( int i = 0; i < count[maxPoint]; ++i )
-    output += input[start+i];
-
-  delete count;
   return output;
 }
 
